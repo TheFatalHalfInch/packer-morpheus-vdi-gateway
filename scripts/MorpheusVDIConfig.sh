f24f5af16476bb6c5c00d9ac9b1753e6d8898c39
TheFatalHalfInch <scagglywag@gmail.com>
2022-01-28 00:27:09 -0700

    script restructure

--- scripts/MorpheusVDIConfig.sh
+++ scripts/MorpheusVDIConfig.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-#reference: https://www.geeksforgeeks.org/bash-scripting-split-string/
+#create array for strings to look for in our file
+finds=("worker_url '" "worker['appliance_url'] = '" "worker['apikey'] = '")
+#file we are altering
+file="/etc/morpheus/morpheus-worker.rb"
 
-#vars
-ips=$(hostname -i)
-file="/etc/morpheus/morpheus.rb"
-find="appliance_url"
+#getting input from user for information to add to config file
+echo "Enter Gateway Worker URL"
+echo "This is the gateway URL that your Morpheus appliance can resolve and reach on 443"
+read gateway
 
-# Set space as the delimiter
-IFS=' '
+echo "Enter Appliance URL"
+echo "The resolvable URL or IP address of your Morpheus appliance which the gateway can reach on 443"
+read appliance
 
-# Read the split words into an array
-# based on space delimiter
-read -ra newarr <<< "$ips"
+echo "Enter API key"
+echo "VDI gateway API key generated from your Morpheus appliance @ VDI Pools > VDI Gateways Configuration"
+read apikey
 
-#foreach line in the new array we created
-for val in "${newarr[@]}"; do
-  #if the line doesn't contain 127.0.*
-  if [[ ! $val =~ 127.0.*$ ]]; then
-    #set ip equal to the line (this should get your current ip address)
-    ip="$val"
-  #end if
-  fi
-#end foreach
+#for every string we are looking to alter
+for find in "${finds[@]}"; do
+  #switch case statement based on $find from foreach loop
+  case $find in
+    #if the string equals this value
+    "worker_url '")
+      #set the append variable equal to this var + an escaped single quote to properly close the sed statement
+      append="${gateway}\'"
+      ;;
+    "worker['appliance_url'] = '")
+      append="${appliance}\'"
+      ;;
+    "worker['apikey'] = '")
+      append="${apikey}\'"
+      ;;
+  #end switch case
+  esac
+  #sed command to find our string and append it with our custom info
+  sed -i "s/${find}.*/${find}${append}/" "$file"
+#end foreach loop
 done
 
-#open the file defined earlier, find the line with the string you are
-#looking for, and replace it with the string you are looking for and
-#'https://(the ip address we found)'
-sed -i "s/$find 'https:\/\/.*/$find \'https:\/\/$ip\'/" "$file"
-
 #reconfigure morpheus now that the ip address has been changed in the file
-morpheus-ctl reconfigure
+morpheus-worker-ctl reconfigure
